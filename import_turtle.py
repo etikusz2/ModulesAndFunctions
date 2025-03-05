536b8bb61e1c5a9054feb8bb5ca4443a2ee56278
Birtalan Etele <etele.birtalan@gmail.com>
2025-03-05 19:04:08 +0200

    importing specific objects

--- import_turtle.py
+++ import_turtle.py
@@ -8,8 +8,7 @@
 # turtle.done()
 
 import turtle
-import math
-
+from math import radians, cos
 
 def square(length: int) -> None:
     """Draws a square of length 'length'"""
@@ -23,15 +22,17 @@ def encircled_square(length: int) -> None:
     then encloses it in a circle
     """
     square(length)
-    angle = math.radians(45)
-    radius = length * math.cos(angle)
+    angle = radians(45)
+    radius = length * cos(angle)
     turtle.right(135)
     turtle.circle(radius)
+    turtle.left(135)
 
 
-encircled_square(300)
-# for s in range(72):
-#     square(120)
-#     turtle.left(5)
+# encircled_square(300)
+turtle.speed('fast')
+for s in range(72):
+    encircled_square(120)
+    turtle.left(5)
 
 turtle.done()
