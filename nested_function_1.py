f6b9fdbaa5c9ec7f03ee2e2c4e7b7f2044a89b5b
Birtalan Etele <etele.birtalan@gmail.com>
2025-03-13 19:37:33 +0200

    Namespace Dictionaries and Free Variable Behavior

--- nested_function_1.py
+++ nested_function_1.py
@@ -1,16 +1,20 @@
 def greet_pythons(items: list) -> None:
     greeting = 'Hello'
     print(f'The ID of `greeting` in `great_pythons` is {id(greeting)}')
+    print(f'local namespace in `greet_pythons`(1): {locals()}')
 
     def make_greeting(item: str) -> str:
+        print(f'local namespace in `greet_pythons`(1): {locals()}')
         greeting = 'Hi '
         print(f'The ID of `greeting` in `great_pythons` is {id(greeting)}')
+        print(f'local namespace in `greet_pythons`(2): {locals()}')
         return f'{greeting} {item}'
 
     for item in items:
         print(make_greeting(item))
     print(f'Inside greet_pythons, `greeting is {greeting}')
     print(f'The ID of `greeting` in `great_pythons` is {id(greeting)}')
+    print(f'local namespace in `greet_pythons`(2): {locals()}')
 
 
 names = ['John'] #, 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
