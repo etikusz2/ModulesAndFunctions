afc50f4c79edc79eddbac923820f6aa88d4e0b99
Birtalan Etele <etele.birtalan@gmail.com>
2025-03-29 20:14:48 +0200

    Understanding the Risks and Uses of 'import *

--- import_turtle.py
+++ import_turtle.py
@@ -7,14 +7,14 @@
 # turtle.right(90)
 # turtle.done()
 
-import turtle
+from  turtle import *
 from math import radians, cos
 
 def square(length: int) -> None:
     """Draws a square of length 'length'"""
     for side in range(4):
-        turtle.forward(length)
-        turtle.right(90)
+        forward(length)
+        right(90)
 
 
 def encircled_square(length: int) -> None:
@@ -24,19 +24,19 @@ def encircled_square(length: int) -> None:
     square(length)
     angle = radians(45)
     radius = length * cos(angle)
-    turtle.right(135)
-    turtle.circle(radius)
-    turtle.left(135)
+    right(135)
+    circle(radius)
+    left(135)
     print(f'Inside function, namespace is: {dir()}')
     print(f'locals: {locals()}')
 
 
-encircled_square(300)
-# turtle.speed('fast')
-# for s in range(72):
-#     encircled_square(120)
-#     turtle.left(5)
-#
-# turtle.done()
+# encircled_square(300)
+speed('fast')
+for s in range(72):
+    encircled_square(120)
+    left(5)
+
+done()
 
 print(dir())
